b16e3561838e55bf54f3c9eb2872b7d39eb4538f
Frank <Frank@192.168.0.102>
2015-07-05 16:01:43 -0430

    primer commit

---
+++ src/Clases/ListadoPersonas.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.util.List;
+
+
+/*
+clase que represeta el listado de personas.
+*/
+public class ListadoPersonas {
+  private List<Persona> listadoPersonas;   
+}
---
+++ src/Clases/Persona.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/*
+clase que representa una persona, puede realizar un registro 
+*/
+public class Persona {
+    private String cedula;//cedula de la persona
+    private int  edad;//edad de la persona
+    private String nombre;//nombre de la persona
+}
